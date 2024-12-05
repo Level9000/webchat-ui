d61bdb4137e152f56c1a28f00329e4257be392e3
Erik Warren <erikwarren@Eriks-Mac-Studio.local>
2024-12-05 15:41:54 -0500

    updated metadata to say OWNFORM.ai

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "OWNFORM.ai Chat"
+const APP_DEFAULT_TITLE = "OWNFORM.ai Chat"
+const APP_TITLE_TEMPLATE = "%s - OWNFORM.ai Chat"
+const APP_DESCRIPTION = "OWNFORM.ai Chat PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
