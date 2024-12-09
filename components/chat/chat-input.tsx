852db1667668703652fabb25eea9695f131e467a
Erik Warren <erikwarren@Eriks-Mac-Studio.local>
2024-12-09 11:41:05 -0500

    commented out the ability to add files

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -212,16 +212,16 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
       </div>
 
       <div className="border-input relative mt-3 flex min-h-[60px] w-full items-center justify-center rounded-xl border-2">
-        <div className="absolute bottom-[76px] left-0 max-h-[300px] w-full overflow-auto rounded-xl dark:border-none">
+        {/* <div className="absolute bottom-[76px] left-0 max-h-[300px] w-full overflow-auto rounded-xl dark:border-none">
           <ChatCommandInput />
-        </div>
+        </div> */}
 
         <>
-          <IconCirclePlus
+          {/* <IconCirclePlus
             className="absolute bottom-[12px] left-3 cursor-pointer p-1 hover:opacity-50"
             size={32}
             onClick={() => fileInputRef.current?.click()}
-          />
+          /> */}
 
           {/* Hidden input to select files from device */}
           <Input
@@ -241,7 +241,7 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
           className="ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring text-md flex w-full resize-none rounded-md border-none bg-transparent px-14 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           placeholder={t(
             // `Ask anything. Type "@" for assistants, "/" for prompts, "#" for files, and "!" for tools.`
-            `Ask anything. Type @  /  #  !`
+            `let's chat.`
           )}
           onValueChange={handleInputChange}
           value={userInput}
