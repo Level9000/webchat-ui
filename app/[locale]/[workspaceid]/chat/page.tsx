a48513dd588b1352f358101181e57cc2d8aa7179
Erik Warren <erikwarren@Eriks-Mac-Studio.local>
2024-12-09 10:55:55 -0500

    removed ChatSettingsForm from chat-settings.tsx

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -36,6 +36,10 @@ export default function ChatPage() {
             <QuickSettings />
           </div>
 
+          <div className="absolute right-2 top-2">
+            <ChatSettings />
+          </div>
+
           <div className="flex grow flex-col items-center justify-center" />
 
           <div className="w-full min-w-[300px] items-end px-2 pb-3 pt-0 sm:w-[600px] sm:pb-8 sm:pt-5 md:w-[700px] lg:w-[700px] xl:w-[800px]">
