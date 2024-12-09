c816766879a9563fa1f7816b7d7689fcb093e4a8
Erik Warren <erikwarren@Eriks-Mac-Studio.local>
2024-12-09 15:13:15 -0500

    modifying styling

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -28,18 +28,12 @@ export default function ChatPage() {
     <>
       {chatMessages.length === 0 ? (
         <div className="relative flex h-full flex-col items-center justify-center">
-          <div className="top-5% left-50% -translate-x-50% -translate-y-5% absolute mb-20">
+          <div className="top-50% left-50% -translate-x-50% -translate-y-50% absolute mb-20">
             <Brand theme={theme === "dark" ? "dark" : "light"} />
           </div>
 
-          <div className="absolute left-2 top-2">
+          <div className="bottom-2 flex grow flex-col items-center justify-center">
             <QuickSettings />
-          </div>
-
-          <div className="absolute right-2 top-2"></div>
-
-          <div className="flex grow flex-col items-center justify-center">
-            <ChatSettings />
           </div>
 
           <div className="w-full min-w-[300px] items-end px-2 pb-3 pt-0 sm:w-[600px] sm:pb-8 sm:pt-5 md:w-[700px] lg:w-[700px] xl:w-[800px]">
